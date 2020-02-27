4c0918a0a474c4d0f221cbf18790813c607e61d8
Stepan <uspita@gmail.com>
2020-02-27 17:08:26 +0300

    Добавьте файлы проекта.

---
+++ Snake/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+<<<<<<< Updated upstream
+            Console.WriteLine("Hello World");
+=======
+            int x1 = 3;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Draw(x1, y1, sym1); //dsfsd
+
+            int x2 = 10;
+            int y2 = 10;
+            char sym2 = '#';
+
+            Draw(x2, y2, sym2);
+
+>>>>>>> Stashed changes
+            Console.ReadLine();
+        }
+    }
+}
