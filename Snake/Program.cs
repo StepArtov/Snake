2bec19c21bdf69d5d2adfa95efc4ee239ed1d1cb
Stepan <uspita@gmail.com>
2020-02-27 17:33:53 +0300

    #удаление функций

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,22 +10,9 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-<<<<<<< Updated upstream
             Console.WriteLine("Hello World");
-=======
-            int x1 = 3;
-            int y1 = 3;
-            char sym1 = '*';
 
-            Draw(x1, y1, sym1); //dsfsd
 
-            int x2 = 10;
-            int y2 = 10;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
-
->>>>>>> Stashed changes
             Console.ReadLine();
         }
     }
