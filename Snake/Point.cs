789ceb8054cdd44bedfffd5f8a73dada1b800314
Stepan <uspita@gmail.com>
2020-03-01 00:54:03 +0300

    aa

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,16 @@ namespace Snake
         public int y;
         public char sym;
 
+        public Point()
+        {
+
+        }
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
