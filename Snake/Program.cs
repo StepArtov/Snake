21315236e929e0ca57adae7c01c718cd28a0778a
Stepan <uspita@gmail.com>
2020-03-01 00:39:54 +0300

    q

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,10 +10,10 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');//
+            Point p1 = new Point(1, 5, '*');
             p1.Draw();
 
-            Point p2 = new Point(2, 5, '*');
+            Point p2 = new Point(4, 4, '*');
 
             p2.Draw();
 
